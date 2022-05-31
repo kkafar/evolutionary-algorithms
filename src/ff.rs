943872e2d933a34230d5e712dcec24c07dd6b635
Cinereo2 <102378409+Cinereo2@users.noreply.github.com>
2022-05-31 15:38:07 +0200

    Update ff.rs
    Algo description, attempt 1

--- src/ff.rs
+++ src/ff.rs
@@ -1,3 +1,61 @@
+/*
+
+Firefly algorithm
+
+Single-threaded implementation, proposed by Xin-She Yang in "Nature-Inspired Metaheuristic
+Algorithms", 2008, ISBN ISBN 978-1-905986-10-1.
+
+Algorithm is roughly inspired by behaviours of fireflies, a light-emitting beetles of Coleoptera
+order. They emit light during twilights as a honest warning signal that they are distasteful.
+
+A single firefly represents a possible - but not necessarily optimal - solution.
+
+Algorithm requires a function referred to as an objective function; value of that function is
+directly proportional to firefly's brightness in maximization problems and inversely proportional in
+minimization problems. That value dictates firefly's brightness.
+
+To begin, we initialize our fireflies' population by randomly placing them within area restricted by
+bound parameters provided, then calculate each firefly's brightness via provided objective function.
+
+The optimization proper can be described in pseudocode as following:
+
+(I x denotes light intensity of given x)
+
+while (t < MaxGeneration)
+        for i = 1 : n (all n fireflies)
+            for j = 1 : i (n fireflies)
+                if ( I j > I i ),
+                    move firefly i towards j;
+                    Evaluate new solutions and update light intensity;
+                end if
+            end for j
+        end for i
+        Rank fireflies and find the current best;
+    end while
+
+Note that the number of objective function evaluations per loop is one evaluation per firefly, even
+though the above pseudocode suggests it is n×n. (Based on Yang's MATLAB code.) Thus the total number
+of objective function evaluations is (number of generations) × (number of fireflies).
+
+A distinctive feature of the firefly algorithm is the phenomenon of automatic division. Since
+attractiveness of solution decreases with distance, which - if reasonable light absorption
+coefficient is employed - results in separation of population into subgroups, with each subgroup
+centered around a different local optimum. Average distance between two adjacent firefly groups can
+be expressed as -f64::powf(f64::sqrt(GAMMA), -1).
+
+This property can be easily visualised by optimizing f(x,y) = (|x| + |y|)e^(-x^2-y^2), with gamma
+parameter of 0.98. There are four local maximums, located in (+- 0.5, +- 0.5). Algorithm
+approximates local optimums within ~ 30 generations, with population counting 25.
+
+Complexity of the algorithm is O(t*n^2), t - no of iterations, n - no of fireflies. For relatively
+large n, double loop should be replaced with a single loop, and the attractiveness of fireflies
+sorted by a comparative algorithm of choice, bronging complexity down to O(n*t*log(n)).
+
+Firefly algorithm has found widespread use in analysis of equilibrium states in chemistry, and as an
+effective solution to semantic syntax problems in computer science.
+
+*/
+
 use std::f64;
 use rand::{Rng, thread_rng};
 
@@ -51,7 +109,7 @@ pub struct FireflyAlgorithm {
 
 }
 
-impl FireflyAlgorithm  {
+impl FireflyAlgorithm {
     fn new(config: FireflyAlgorithmCfg, brightness_function: fn(&Vec<f64>) -> f64, probe: Box<dyn Probe>) -> Self {
         FireflyAlgorithm {
             config,
@@ -78,7 +136,7 @@ impl FireflyAlgorithm  {
         let scale = self.config.upper_bound - self.config.lower_bound;
         let mut alfa = self.config.alfa0;
         let mut rng = thread_rng();
-        let mut currentbest:f64 = f64::MAX;
+        let mut currentbest: f64 = f64::MAX;
         for generation in 0..self.config.max_generations {
             if generation % 25 == 0 {
                 self.probe.on_iteration_start(&generation)
@@ -107,9 +165,8 @@ impl FireflyAlgorithm  {
                         maxbright = brightness[index];
                         maxpos = index;
                     }
-
                 }
-                if (self.brightness_function)(&population[maxpos]) < currentbest{
+                if (self.brightness_function)(&population[maxpos]) < currentbest {
                     self.probe.on_new_best(&(self.brightness_function)(&population[maxpos]));
                     currentbest = (self.brightness_function)(&population[maxpos]);
                 } else {
@@ -117,7 +174,7 @@ impl FireflyAlgorithm  {
                 }
                 //println!("Gen: {}, x: {}, y: {}", generation, population[maxpos][0], population[maxpos][1]);
             }
-            if generation % 25 == 0{
+            if generation % 25 == 0 {
                 //self.probe.on_iteration_end(&generation); //TODO CHYBA TEGO NIE POTRZEBUJĘ
                 println!();//TODO PO PROSTU WYPISZĘ NEWLINE USUŃ TO
             }
